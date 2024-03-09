2478d6f36976b93a57826aeca57c042ada2d9f34
Bruno-Ambrosio <brunoalvesambrosio@gmail.com>
2024-03-08 21:57:21 -0300

    seed with Faker

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "https://rubygems.org"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.3"
 
+gem "cpf_cnpj"
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
@@ -30,6 +32,7 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 gem "redis", ">= 4.0.1"
 
+gem "faker"
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
 User.destroy_all
+Person.destroy_all
+Debt.destroy_all
 
-User.create email: 'admin@admin.com', password: '111111'
+User.create(email: 'admin@admin.com', password: '123456') 
+#People.create name: 'Admin', national_id: '11111111111', cpf_or_cnpj: '11111111111', active: true
 
-puts "Usuário criado:"
-puts "login admin@admin.com"
-puts "111111"
+50.times do
+    User.create(
+      email: Faker::Internet.email,
+      password: Faker::Internet.password
+    )
+end
+user_ids = User.pluck(:id)
+
+100.times do
+    Person.create(
+      name: Faker::Name.name,
+      phone_number: Faker::PhoneNumber.phone_number,
+      national_id: CPF.generate,
+      active: Faker::Boolean.boolean(true_ratio: 0.5),
+      user_id: user_ids.sample
+    )
+end
+person_ids = Person.pluck(:id)
+
+500.times do
+    Debt.create(
+      person_id: person_ids.sample,
+      amount: rand(1..1000),
+      observation: Faker::Lorem.sentence
+    )
+end
